87f23ec4cf8441287f5c226f59d4d6157b91fb5c
James Hartig <fastest963@gmail.com>
2010-12-31 21:49:03 -0500

    Up timeouts in extension and test listen.php

--- listen.php
+++ listen.php
@@ -1,16 +1,41 @@
 <?php
+
+ignore_user_abort(true); //we can disable this but for testing I want to see -
+//set_time_limit(5); //this doesn't do shit
+ob_implicit_flush(true);
+
 //this is the inbox for versions >=0.6
 require(".config.php");
 require("master.php");
 require("db.php");
 
-set_time_limit(5); //this doesn't do shit
-ob_implicit_flush(true);
-
 if (!headers_sent()) {
     header('Access-Control-Allow-Origin: *');
 }
 
+//begin testing
+$x = 0;
+while (true) {
+    sleep(1);
+    $x++;
+    //somehow this combination of flushes gets php to check the connection status.
+    ob_end_flush();
+    flush();
+    ob_end_clean();
+    if(connection_status() != CONNECTION_NORMAL)
+    {
+        file_put_contents("/tmp/log.cb","-",FILE_APPEND);
+        exit(0);
+    } else {
+        file_put_contents("/tmp/log.cb",connection_status(),FILE_APPEND);
+    }
+    if ($x > 15) {
+        file_put_contents("/tmp/log.cb",5,FILE_APPEND);
+        exit(0);
+    }
+}
+
+//begin normal script
 function postInvalidToken() {
     if (isset($_GET['post'])) {
         die("invalid_token");
@@ -36,7 +61,11 @@ if (isset($_GET['token'])) {
             //$db->unsubscribe($user); //run this last, we don't know what it will do
             exit(0);
         }
-        $db->subscribe($user, 'gotItem');        
+        
+        echo "-"; //start output
+        flush();
+        ob_flush();
+        //$db->subscribe($user, 'gotItem');
         
     } else {
         postInvalidToken();
